8dcb443b40903e3b3a6e0ed928896cd5b745eaae
blakewallan <blakewallan@gmail.com>
2016-01-30 13:13:47 -0800

    Added working user auth

--- index.js
+++ index.js
@@ -4,17 +4,44 @@ var express     = require('express'),
     path        = require('path'),
     bodyParser  = require('body-parser'),
     _           = require('lodash'),
+    passport    = require('passport'),
+    http        = require('http'),
+    LocalStrategy = require('passport-local').Strategy,
+    methodOverride = require('method-override'),
+    cookieParser = require('cookie-parser'),
+    session     = require('express-session'),
 
-  app         = express();
+    app         = express();
 
 
-mongoose.connect('mongodb://localhost:27017/prioritize-travel' || process.env.MONGOLAB_URI);
-mongoose.connection.once('open', function(){
+  mongoose.connect('mongodb://localhost:27017/prioritize-travel' || process.env.MONGOLAB_URI);
+  mongoose.connection.once('open', function(){
 
   //Load DB models
   app.models = require('./server/models/index');
 
+  //Point to Angular Dir
   app.use(express.static(path.join(__dirname, 'client')));
+
+  app.use(bodyParser.urlencoded({extended: true}));
+
+
+  //TODO: Do I need this?
+  //app.use(cookieParser('mYsEcReT'));
+  app.use(session({
+    secret: 'sasdlfkajsldfkajweoriw234234ksdfjals23',
+    resave: false,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // passport config
+  var User = require('./server/models/user');
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser())
+
 
   // Load the routes.
   var routes = require('./server/routes');
