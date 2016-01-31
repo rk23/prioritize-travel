8b33caa9713adfda736872747affffafc0642232
blakewallan <blakewallan@gmail.com>
2016-01-30 18:06:07 -0800

    Working on top spots functionality

--- server/helpers/expediaApiHelper.js
+++ server/helpers/expediaApiHelper.js
@@ -4,6 +4,19 @@ var request = require('request');
 
 module.exports = {
 
+  topSpots : [
+    'CDG',
+    'JFK',
+    'FCO',
+    'CUN',
+    'LCY',
+    'MIA',
+    'MCO',
+    'SFO',
+    'MYR',
+    'BKG'
+  ],
+
   unrealDeals : function(origin, destination, callback){
 
     var uri = 'http://terminal2.expedia.com:80/x/deals/packages?originTLA='+ origin +'&destinationTLA='+ destination +'&startDate=2016-03-01&endDate=2016-12-31&lengthOfStay=5&roomCount=1&adultCount=1&childCount=0&infantCount=0&apikey=BAAGqgon5IWIpBxkrprhYzQDY4bZpPlE';
@@ -27,6 +40,17 @@ module.exports = {
       }
       callback({average : runningTotal / unrealDeals.length});
     });
+  },
+
+  topSpotsCost : function(origin, topSpots, callback){
+
+    for(var i = 0; i < topSpots.length; i ++){
+      var uri = 'http://terminal2.expedia.com:80/x/deals/packages?originTLA='+ origin +'&destinationTLA='+ topSpots[i] +'&startDate=2016-03-01&endDate=2016-12-31&lengthOfStay=5&roomCount=1&adultCount=1&childCount=0&infantCount=0&apikey=BAAGqgon5IWIpBxkrprhYzQDY4bZpPlE';
+      request(uri, function(req, res){
+
+      })
+    }
+
 
   }
 
