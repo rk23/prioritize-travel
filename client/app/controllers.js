be4ebb568bf63fffe01da6f750e6f3353c9cdb66
rk23 <reed.kinning@gmail.com>
2016-01-30 16:10:01 -0800

    userctrl on profile view and controller username

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -56,12 +56,11 @@ angular.module('HackathonCtrls', [])
   }])
   .controller('LoginCtrl', ['$scope', '$http', '$location', '$rootScope', function($scope, $http, $location, $rootScope){
     $scope.user = {
-      email: '',
+      username: '',
       password: ''
     }
 
     $scope.userLogin = function(){
-
       $http({
         method: 'POST',
         url: 'auth/login',
@@ -82,31 +81,6 @@ angular.module('HackathonCtrls', [])
 
     }
   }])
-  .controller('UserCtrl', ['$scope', '$http', '$location', '$rootScope', function($scope, $http, $location, $rootScope){
-    $scope.user = {
-      email: '',
-      password: ''
-    }
+  .controller('UserCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
 
-    $scope.userLogin = function(){
-
-      $http({
-        method: 'POST',
-        url: 'auth/login',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        },
-        data: $scope.user
-      }).success(function (res) {
-        $rootScope.loggedIn = true;
-        $location.path('/')
-      }).error(function(res) {
-        console.log(res);
-      });
-
-    }
   }]);
